26e173d4046569e298414e550e66877f7aeea260
Alex Hernandez <alexish4@cpp.edu>
2021-09-24 20:30:56 -0700

    Finished the code

--- RestaurantBill.c
+++ RestaurantBill.c
@@ -22,6 +22,7 @@ int main(void) //Restaurant Bill Function
     double pizza = 22.35;
     char foodName[4][10] = {"salad", "soup", "sandwich", "pizza"}; //creating a string array that lines up with array of price
     double food[4] = {salad, soup, sandwich, pizza}; //storing the price of the food
+    int number = 0; //holds place of random number for arrays
 
     double tax = 0.06;
     double tip;
@@ -32,10 +33,16 @@ int main(void) //Restaurant Bill Function
     time_t t;
     srand((unsigned) time(&t));
 
-    int number = rand() % 4;
+    int randNumber = rand() % 4;
+    while(number < randNumber)
+    {
+        number++;
+    }
+    int stringNumber = number - 1; // I have no idea but for some reason I had to decrease the number by 1 to get the arrays in sync. 
+                                    //It took me awhile to notice the pattern
     double total = food[number] + (food[number] * tipValue) + (food[number] * tax);
-    printf("Your bill for the %s is %lf, so in total including the tax and tip, you owe %lf\n", 
-                                            &foodName[number][10], food[number], total);
+    printf("Your bill for the %s is %0.2lf, so in total including the tax and tip, you owe %0.2lf\n", 
+                                            &foodName[stringNumber][10], food[number], total);
 
     
     return 0;
